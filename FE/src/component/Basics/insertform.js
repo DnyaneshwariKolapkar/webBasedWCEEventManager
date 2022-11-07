182043baba7d7780253a296057a66ccc8d7021cd
RiteshWanave <riteshwanave@gmail.com>
2022-11-07 12:38:21 +0530

    time picker changes

--- FE/src/component/Basics/insertform.js
+++ FE/src/component/Basics/insertform.js
@@ -2,7 +2,6 @@ import { React, useState } from 'react'
 import './insertform.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import TimePicker from 'react-time-picker';
 
 function Insertform( {eventDate} ) {
     const [eventName, setEventName] = useState('');
@@ -41,15 +40,14 @@ function Insertform( {eventDate} ) {
         catch(err) {
             console.log(err);
         }
-    }
+    }   
 
-    const [ time, setTime ] = useState('10:00');
     const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     return (
         <>
             <div className='insertformbody'>
                 <div>
-                    <form onSubmit={SubmitButton} action=' '>
+                    <div className='form'>
                         <h1>Book Slot on {eventDate.getDate().toString() + " " + month[eventDate.getMonth()] + " " + eventDate.getFullYear().toString()} </h1>
                         <br /><br />
                         <input className='insertformbodyinput' type="text" placeholder="Event Name" value={eventName} onChange={(e) => setEventName(e.target.value)} />
@@ -59,10 +57,9 @@ function Insertform( {eventDate} ) {
                         <input className='insertformbodyinput' type="text" placeholder="Event Link" value={eventLink} onChange={(e) => setEventLink(e.target.value)} />
                         <input className='insertformbodyinput' type="text" placeholder="Description" value={about} onChange={(e) => setAbout(e.target.value)} />
                         <br />
-                        <button type="submit" className='buttonloginpage' >Submit</button>   
-                    </form>
+                        <button type="submit" className='buttonloginpage' onClick={SubmitButton} >Submit</button>   
+                    </div>
                 </div>
-                <TimePicker value={time} onChange={(time) => setTime(time) } />
             </div>
         </>
     )
